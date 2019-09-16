cf45e7ac7e8bb731961b41e246e716ef48e96a80
Chris Kerbo <chris.kerbo@outlook.com>
2019-09-16 14:58:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name)
+  batch = []
+  name.each {|person| batch << "Hello, my name is #{person}."}
+  batch
+end
